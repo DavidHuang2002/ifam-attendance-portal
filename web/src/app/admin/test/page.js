09cc5935b2e6a07d15a36b2a565ff9c17c7923e1
David Huang <davidhuang1203@hotmail.com>
2024-03-04 10:42:40 -0600

    add examples for tutorial on API

--- web/src/app/admin/test/page.js
+++ web/src/app/admin/test/page.js
@@ -9,11 +9,24 @@ export default function Test(){
         <div>
         <Button onClick={
             async () => {
+                // approach1: use define a function in service that calls API
+                // separate the logic with view
                 const res = await postAttednace("xx", "david.test@x.com")
                 console.log(res);
 
                 const docRef = await res.json();
                 console.log(docRef);
+                
+                // approach2: a quick and easy way - directly call the API
+                // const response = await fetch('/api/attendance', {
+                //     method: 'POST',
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify({
+                //         event: "xx",
+                //         email: "ss"})
+                // });
             }
         }>Test</Button>
         </div>
