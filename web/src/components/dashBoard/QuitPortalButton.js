4ac813c0baf5b2c1b62b881df275462dfe20e3c7
David Huang <davidhuang1203@hotmail.com>
2024-03-07 02:24:34 -0600

    use absolute path for quitting portal

--- web/src/components/dashBoard/QuitPortalButton.js
+++ web/src/components/dashBoard/QuitPortalButton.js
@@ -1,20 +1,20 @@
-
-import React from 'react';
-import { Button } from 'antd';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { Button } from "antd";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export function QuitPortal() {
-  return(
-    <nav className = 'flex-between w-full mb-16 pt-3'>
-      <div style={{position: 'fixed', bottom: 60, left: 50, textAlign: 'center',}}>
-      <Link href='../' className='black_btn' >
-        <button type = "dashed" ghost>
-          Quit Portal
-        </button>
-       
-      </Link>
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <div
+        style={{ position: "fixed", bottom: 60, left: 50, textAlign: "center" }}
+      >
+        <Link href="/" className="black_btn">
+          <button type="dashed" ghost>
+            Quit Portal
+          </button>
+        </Link>
       </div>
-      </nav>
-);
+    </nav>
+  );
 }
