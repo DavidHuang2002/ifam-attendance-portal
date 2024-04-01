a86fcccb4b07a27fd4e398b51444aab18bd9fa5d
David Huang <davidhuang1203@hotmail.com>
2024-03-31 21:39:24 -0500

    attendance and export feature for past events

--- web/src/app/admin/events/past/page.js
+++ web/src/app/admin/events/past/page.js
@@ -1,84 +1,127 @@
-"use client"
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import { LayoutSider } from '@/components/dashBoard/LayoutSider';
-import { Layout, Button } from 'antd';
+"use client";
+import React from "react";
+import { Space, Table, Tag } from "antd";
+import { LayoutSider } from "@/components/dashBoard/LayoutSider";
+import { Layout, Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+import { EVENTS_RECORD, get_event_record_export_route } from "@/constants/api-endpoints";
+import { useState, useEffect } from "react";
+import EventAttendanceModal from "@/components/pastEvents/EventAttendanceModal";
 
 const { Content } = Layout;
 
-const columns = [
-  {
-    title: 'Event Name',
-    dataIndex: 'EventName',
-    key: 'EventName',
-    align: 'center',
-  },
-  {
-    title: 'Date',
-    dataIndex: 'Date',
-    key: 'Date',
-    align: 'center',
-  },
-  {
-    title: '# Participants',
-    dataIndex: 'num_P',
-    key: 'num_P',
-    align: 'center',
-  },
-  {
-    title: '# RSVP',
-    dataIndex: 'num_RSVP',
-    key: 'num_RSVP',
-    align: 'center',
-  },
-  {
-    title: 'Export Data',
-    dataIndex: 'Export',
-    key: 'Export',
-    align: 'center',
-  },
-];
-const data = [
-  {
-    EventName: 'I-FAM Dinner',
-    Date: 'xx/xx/xxxx',
-    num_P: 15,
-    num_RSVP: 25,
-    // tags: ['nice', 'developer'],
-    Export: <Button>Export</Button>,
-  },
-  {
-    EventName: 'I-FAM Event 2',
-    Date: 'xx/xx/xxxx',
-    num_P: 10,
-    num_RSVP: 18,
-    // tags: ['loser'],
-    Export: <Button>Export</Button>,
-  },
-  {
-    EventName: 'I-FAM Event 3',
-    Date: 'xx/xx/xxxx',
-    num_P: 17,
-    num_RSVP: 20,
-    // tags: ['cool', 'teacher'],
-    Export: <Button>Export</Button>,
-  },
-  {
-    EventName: 'I-FAM Event 4',
-    Date: 'xx/xx/xxxx',
-    num_P: 7,
-    num_RSVP: 15,
-    // tags: ['cool', 'teacher'],
-    Export: <Button>Export</Button>,
-  },
-];
+const fetchPastEvents = async () => {
+  const response = await fetch(EVENTS_RECORD);
+  const data = await response.json();
+
+  data.forEach((event) => {
+    event.key = event.id;
+    event.eventDate = new Date(event.eventDates[0]).toDateString();
+  });
+  return data;
+};
+
 export default function ManagePastEvents() {
-    return (
-        <LayoutSider>
-            <Content style={{ marginLeft: '200px', padding: '24px', minHeight: '100vh' }}>
-                <h1>Past events</h1>
-                <Table columns={columns} dataSource={data} />
-            </Content>
-        </LayoutSider>
-    );
+  const [data, setData] = useState([]);
+  const [attendanceModalOpen, setAttendanceModalOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  useEffect(() => {
+    fetchPastEvents().then((data) => setData(data));
+  }, []);
+
+  const downloadEventRecord = async (eventId) => {
+    const response = await fetch(get_event_record_export_route(eventId));
+    const blob = await response.blob(); // Convert the response to a Blob
+    let filename = "download.csv"; // Default filename if not specified
+
+    // Attempt to extract the filename from the Content-Disposition header
+    const contentDisposition = response.headers.get("Content-Disposition");
+    if (contentDisposition) {
+      const match = contentDisposition.match(/filename="?(.+)"?/);
+      console.log("match", match)
+      if (match && match[1]) {
+        // trim the quotes from the filename
+        filename = match[1].replace(/"/g, "");
+      }
+    }
+
+    const url = window.URL.createObjectURL(blob); // Create a URL for the blob
+    const a = document.createElement("a"); // Create an <a> element
+    a.href = url; // Set the href of the <a> element to the blob URL
+    a.download = filename; // Set the download attribute to the filename
+    document.body.appendChild(a); // Append the <a> element to the body
+    a.click(); // Programmatically click the <a> element to start the download
+    a.remove(); // Remove the <a> element from the DOM
+  };
+
+  const columns = [
+    {
+      title: "Event Name",
+      dataIndex: "eventName",
+      align: "center",
+    },
+    {
+      title: "Date",
+      dataIndex: "eventDate",
+      align: "center",
+    },
+    {
+      title: "# Participants",
+      dataIndex: "participantNumber",
+      align: "center",
+    },
+    {
+      title: "# RSVP",
+      dataIndex: "rsvpNumber",
+      align: "center",
+    },
+    {
+      title: "See Attendance",
+      align: "center",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button
+            onClick={() => {
+              setSelectedEvent(record.eventId);
+              setAttendanceModalOpen(true);
+            }}
+          >
+            Attendance
+          </Button>
+        </Space>
+      ),
+    },
+    {
+      title: "Export",
+      align: "center",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button
+            onClick={() => {
+              downloadEventRecord(record.eventId);
+            }}
+          >
+            <DownloadOutlined />
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <LayoutSider>
+      <Content
+        style={{ marginLeft: "200px", padding: "24px", minHeight: "100vh" }}
+      >
+        <h1>Past events</h1>
+        <Table columns={columns} dataSource={data} />
+        <EventAttendanceModal
+          eventId={selectedEvent}
+          open={attendanceModalOpen}
+          setOpen={setAttendanceModalOpen}
+        />
+      </Content>
+    </LayoutSider>
+  );
 }
