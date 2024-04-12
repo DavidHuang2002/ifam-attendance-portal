30212c0d3b702dc8ff6b4d7dda6cc9f339a175a4
David Huang <davidhuang1203@hotmail.com>
2024-04-11 22:30:19 -0500

    link rsvp to db logic

--- web/src/components/event/RsvpModal.jsx
+++ web/src/components/event/RsvpModal.jsx
@@ -1,8 +1,10 @@
 
-import { Modal, Button, Form, Input } from "antd";
+import { Modal, Button, Form, Input, Spin, message } from "antd";
 import { useAtom } from "jotai";
 import { RsvpModalOpenAtom } from "@/jotaiStore/store";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
+
+import { createRSVP } from "@/service/back-end/rsvp";
 
 export default function RsvpModal({ eventId, eventName}) {
   const [open, setOpen] = useAtom(RsvpModalOpenAtom);
@@ -10,11 +12,33 @@ export default function RsvpModal({ eventId, eventName}) {
 
   const [email, setEmail] = useState("");
   const [showMessage, setShowMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    setEmail("");
+    setShowMessage(false);
+    setLoading(false);
+  }, [open]);
+
+  const submitRSVP = async (email, eventId) => {
+    console.log("RSVP submitted:", email);
+    setLoading(true);
+    
+    try{
+      const docRef = await createRSVP(email, eventId);
+      setLoading(false);
+      setShowMessage(true);
+
+      console.log("RSVP created:", docRef);
+    } catch (error) {
+      console.error("Failed to submit RSVP:", error);
+      message.error("Failed to submit RSVP. Please try again later.");
+      setLoading(false);
+    }
+  };
 
   const handleSubmit = () => {
-    // createRSVP(email, eventId);
-    console.log("RSVP submitted:", email);
-    setShowMessage(true);
+    submitRSVP(email, eventId);
   };
 
   const handleCancel = () => {
@@ -42,6 +66,7 @@ export default function RsvpModal({ eventId, eventName}) {
           </Button>,
         ]}
       >
+        <Spin spinning={loading} tip="Submitting your RSVP...">
         <Form>
           <p>
             RSVP for <b>{eventName}</b>
@@ -62,6 +87,7 @@ export default function RsvpModal({ eventId, eventName}) {
             Thank you for your RSVP. We have received your email: {email}
           </p>
         )}
+        </Spin>
       </Modal>
     </div>
   );
