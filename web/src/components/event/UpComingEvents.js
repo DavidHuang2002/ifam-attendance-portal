67e1f794dd6950f9e2502d9ba87acf8f1603cef7
habonemsk <habone.mohamed.said@gmail.com>
2024-04-15 14:05:50 -0500

    email-gmail
    email-gmail

--- web/src/components/event/UpComingEvents.js
+++ web/src/components/event/UpComingEvents.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Button, message } from "antd";
-import emailjs from "emailjs-com";
+//import emailjs from "emailjs-com";
 import { collection, getDocs } from "firebase/firestore";
 
 import { db } from "@/firebase/config"; // Ensure this path matches your Firebase config
@@ -46,35 +46,7 @@ export function UpcomingEvents({ admin = false }) {
     setRsvpModalOpen(true);
   };
 
-  const notifyAllParticipants = async () => {
-    try {
-      const participantsColRef = collection(db, "participants");
-      const querySnapshot = await getDocs(participantsColRef);
-      const participantsList = querySnapshot.docs
-        .map((doc) => doc.data().email)
-        .filter((email) => email && email.trim() !== ""); // Filter out empty emails
-
-      const sendEmailPromises = participantsList.map((email) => {
-        const templateParams = {
-          email, // Other parameters your email template requires
-        };
-        return emailjs
-          .send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID)
-          .then((response) =>
-            console.log("Email successfully sent to:", email, response)
-          )
-          .catch((error) =>
-            console.error("Failed to send email to:", email, error)
-          );
-      });
-
-      await Promise.all(sendEmailPromises);
-      message.success("All participants with valid emails have been notified!");
-    } catch (error) {
-      console.error("Error sending notifications:", error);
-      message.error("Failed to notify some participants.");
-    }
-  };
+  
 
   const getAdminActions = (event) => {
     // For now we assume that an event is notify-able, so admin can notify whenever they want
@@ -98,29 +70,20 @@ export function UpcomingEvents({ admin = false }) {
           <span>Attendance</span>
         </span>
       </Link>,
+      <Link
+      href={`/admin/event/${event.eventId}/edit/emailparticipants`}
+      key="emailparticipants"
+      passHref
+    >
+      <span className="ant-btn ant-btn-link" style={{ padding: 0 }}>
+        <TeamOutlined />
+        <span>Email</span>
+      </span>
+    </Link>,
     ];
 
     // For actions like "Notify" that don't require navigation
-    if (isNotifyEnabled) {
-      actions.push(
-        <span
-          onClick={() => notifyAllParticipants(event.eventId)}
-          key="notify"
-          className="ant-btn ant-btn-link"
-          style={{ padding: 0, cursor: "pointer" }}
-        >
-          <MailOutlined />
-          <span>Email</span>
-        </span>
-      );
-    } else {
-      actions.push(
-        <span key="notify-disabled" style={{ opacity: 0.4 }}>
-          <MailOutlined />
-          <span>Email</span>
-        </span>
-      );
-    }
+    
 
     actions.push(
       <span key="more">
@@ -140,7 +103,7 @@ export function UpcomingEvents({ admin = false }) {
     ];
   }
 
-
+  
   return (
     <div id="events">
       <h1 style={{ marginBottom: "20px" }}>Our Upcoming Events</h1>
@@ -155,6 +118,7 @@ export function UpcomingEvents({ admin = false }) {
             padding: "30px",
           }}
         >
+
           {events.length > 0 ? (
             events.map((event, index) => (
               <EventCard
