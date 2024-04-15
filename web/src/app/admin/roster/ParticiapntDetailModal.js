1871b78ac7f0516e0ed570292f06ac1437ef435d
David Huang <davidhuang1203@hotmail.com>
2024-04-14 22:16:15 -0500

    ee

--- web/src/app/admin/roster/ParticiapntDetailModal.js
+++ web/src/app/admin/roster/ParticiapntDetailModal.js
@@ -2,51 +2,50 @@ import { useState, useEffect } from 'react';
 import { Modal, Button, Table } from 'antd';
 // import EditInfo from '@/components/roster/EditInfo';
 import { getAllParticipants } from '@/service/back-end/participant';
-import { render } from '@testing-library/react';
 
-const participantInfo = [
-  {
-    title: "Class",
-    dataIndex: "class",
-    key: "class",
-    align: "center",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-    align: "center",
-  },
-  {
-    title: "Interest",
-    dataIndex: "interests",
-    key: "interests",
-    align: "center",
-    // the render function render the interests list as a list of Tags
-    // render the tags in a vertical row
-    // render: (interests) => {
-    //   return (
-    //     <div>
-    //       {interests?.map((interest) => (
-    //         <Tag key={interest} color="blue">
-    //           {interest}
-    //         </Tag>
-    //       ))}
-    //     </div>
-    //   );
-    // }
-  },
-  {
-    title: "Note",
-    dataIndex: "note",
-    key: "note",
-    align: "center",
-    // make sure the note section is not too small by having min width
-    render: (note) => {
-      return <div style={{ minWidth: "200px" }}>{note}</div>;
-    }
-  },
-];
+// const participantInfo = [
+//   {
+//     title: "Class",
+//     dataIndex: "class",
+//     key: "class",
+//     align: "center",
+//   },
+//   {
+//     title: "Email",
+//     dataIndex: "email",
+//     key: "email",
+//     align: "center",
+//   },
+//   {
+//     title: "Interest",
+//     dataIndex: "interests",
+//     key: "interests",
+//     align: "center",
+//     // the render function render the interests list as a list of Tags
+//     // render the tags in a vertical row
+//     // render: (interests) => {
+//     //   return (
+//     //     <div>
+//     //       {interests?.map((interest) => (
+//     //         <Tag key={interest} color="blue">
+//     //           {interest}
+//     //         </Tag>
+//     //       ))}
+//     //     </div>
+//     //   );
+//     // }
+//   },
+//   {
+//     title: "Note",
+//     dataIndex: "note",
+//     key: "note",
+//     align: "center",
+//     // make sure the note section is not too small by having min width
+//     render: (note) => {
+//       return <div style={{ minWidth: "200px" }}>{note}</div>;
+//     }
+//   },
+// ];
 
 export default function ParticipantDetailModal({
   open,
@@ -54,19 +53,19 @@ export default function ParticipantDetailModal({
   participantData,
   onEditSave,
 }) {
-  const [editModalVisible, setEditModalVisible] = useState(false);
-  const showEditModal = () => {
-    setEditModalVisible(true);
-  };
+  // const [editModalVisible, setEditModalVisible] = useState(false);
+  // const showEditModal = () => {
+  //   setEditModalVisible(true);
+  // };
 
-  const handleEditCancel = () => {
-    setEditModalVisible(false);
-  };
+  // const handleEditCancel = () => {
+  //   setEditModalVisible(false);
+  // };
 
-  const handleEditSave = () => {
-    setEditModalVisible(false);
-    onEditSave();
-  };
+  // const handleEditSave = () => {
+  //   setEditModalVisible(false);
+  //   onEditSave();
+  // };
 
 
   return (
@@ -85,6 +84,7 @@ export default function ParticipantDetailModal({
       ) : (
         <p>Loading...</p>
       )} */}
+      <div><p> test</p></div>
     </Modal>
     {/* <EditInfo
     participant={participantData}
